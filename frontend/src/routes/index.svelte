6c20dbc4984d8d3d2fc28c2ee7a94ad3b93d2cd6
miluge <gllmblndl@gmail.com>
2021-05-20 13:08:33 +0200

    Setup Tailwind JIT

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-4xl font-bold bg-black">Welcome to SvelteKit</h1>
+<p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
